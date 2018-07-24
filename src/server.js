02bc3a7a96af9606f4b1e9ee195f493eb0575b03
libingxin <libingxin123456@qq.com>
2018-07-24 11:16:57 +0800

    服务器搭建ok，添加了json文件

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const indexData = require('./json/index')
+
+app.use('/',(req,res)=>{
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers','Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods','PUT, POST, GET, DELETE, OPTIONS');
+  res.json(indexData)
+});
+
+app.listen(3000,()=>{
+  console.log('server is ready on port 3000.')
+})
