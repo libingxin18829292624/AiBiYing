18b1d83a2f7df0b8b4a12ecc89d9711fa4939830
huanglelele <872194944@qq.com>
2018-07-25 19:29:41 +0800

    serach页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import abyIndex from '../pages/abyIndex'
-
+import abySerach from "../pages/abySerach"
 Vue.use(VueRouter)
 
 const routes = [
   {
     path:'/index',
     component:abyIndex
+  },
+  {
+    path:'/serach',
+    component:abySerach
   }
 ]
 
