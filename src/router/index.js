42bc9d69c12b34a4e545430470738d9ee7f2c57b
libingxin <libingxin123456@qq.com>
2018-07-25 14:42:01 +0800

    修改了index.html部分内容

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import abyIndex from '../pages/abyIndex'
+import abyOrder from '../pages/abyOrder'
 
 Vue.use(VueRouter)
 
@@ -8,6 +9,10 @@ const routes = [
   {
     path:'/index',
     component:abyIndex
+  },
+  {
+    path:'/order',
+    component:abyOrder
   }
 ]
 
